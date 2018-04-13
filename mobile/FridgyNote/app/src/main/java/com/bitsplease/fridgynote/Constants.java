f302dbb38d3d47860e5f4b690b8be1f3f91151c1
ElTostas <barros.andrer@gmail.com>
2018-04-13 17:41:45 +0100

    login started and added main activity

---
+++ mobile/FridgyNote/app/src/main/java/com/bitsplease/fridgynote/Constants.java
@@ -0,0 +1,12 @@
+package com.bitsplease.fridgynote;
+
+/**
+ * Created by André on 13/04/2018.
+ */
+
+public class Constants {
+
+    public static final String KEY_USERNAME  = "email";
+
+    public static final String KEY_PASSWORD = "password";
+}
--- mobile/FridgyNote/app/src/main/java/com/bitsplease/fridgynote/LoginActivity.java
+++ mobile/FridgyNote/app/src/main/java/com/bitsplease/fridgynote/LoginActivity.java
@@ -1,13 +1,67 @@
 package com.bitsplease.fridgynote;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    SharedPreferences sharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        if(!sharedPreferences.getString(Constants.KEY_USERNAME, "").equals("")){
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }
+
         setContentView(R.layout.activity_login);
+
+        Button clickButton = findViewById(R.id.buttonLogin);
+        clickButton.setOnClickListener( new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                //TODO cenas de login genericas
+
+                //Field checks
+                EditText email = findViewById(R.id.loginUser);
+                EditText pass = findViewById(R.id.loginPass);
+
+                if(TextUtils.isEmpty(email.getText())){
+                    email.requestFocus();
+                    email.setError("Username field cannot be empty");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass.getText())){
+                    pass.requestFocus();
+                    pass.setError("Password field cannot be empty");
+                    return;
+                }
+
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putString(Constants.KEY_USERNAME, String.valueOf(email.getText()));
+                editor.putString(Constants.KEY_PASSWORD, String.valueOf(pass.getText()));
+                editor.apply();
+
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+
     }
+
+
 }
